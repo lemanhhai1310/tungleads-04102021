470241f8cf7ed69830a6f54b70cdfb0b5b5cc82e
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-10-18 13:57:51 +0700

    update

--- index.php
+++ index.php
@@ -4,7 +4,59 @@
 <div class="home__banner uk-position-relative uk-background-norepeat uk-background-cover uk-background-center-center" data-src="images/bg_banner.png" uk-img>
     <div class="uk-position-cover home__banner__overlay"></div>
     <div class="uk-section-xsmall uk-position-relative">
+        <div class="uk-container uk-padding-remove">
+            <div class="uk-flex-middle" uk-grid>
+                <div class="uk-width-expand">
+                    <div class="uk-padding-small home__banner__box">
 
+                    </div>
+                </div>
+                <div class="uk-width-2-5@m">
+                    <div class="uk-card home__banner__card uk-box-shadow-small">
+                        <div class="uk-card-header">
+                            <div class="home__banner__card__txt1">ĐĂNG KÝ</div>
+                            <div class="home__banner__card__txt2">(Chương trình Hội Viên)</div>
+                        </div>
+                        <div class="uk-card-body">
+                            <form>
+
+                                <div class="uk-margin-small">
+                                    <div class="uk-position-relative uk-display-block">
+                                        <span class="uk-form-icon" uk-icon="icon: user"></span>
+                                        <input class="uk-input home__banner__card__input" type="text" placeholder="Nhập họ và tên...">
+                                    </div>
+                                </div>
+                                <div class="uk-margin-small">
+                                    <div class="uk-position-relative uk-display-block">
+                                        <span class="uk-form-icon" uk-icon="icon: mail"></span>
+                                        <input class="uk-input home__banner__card__input" type="text" placeholder="Nhập địa chỉ Email...">
+                                    </div>
+                                </div>
+                                <div class="uk-margin-small">
+                                    <div class="uk-position-relative uk-display-block">
+                                        <span class="uk-form-icon" uk-icon="icon: receiver"></span>
+                                        <input class="uk-input home__banner__card__input" type="text" placeholder="Nhập số điện thoại/Zalo...">
+                                    </div>
+                                </div>
+                                <div class="uk-margin-small">
+                                    <div class="uk-position-relative uk-display-block">
+                                        <span class="uk-form-icon" uk-icon="icon: lock"></span>
+                                        <input class="uk-input home__banner__card__input" type="text" placeholder="Nhập mật khẩu...">
+                                    </div>
+                                </div>
+                                <div class="uk-margin-small">
+                                    <div class="uk-position-relative uk-display-block">
+                                        <span class="uk-form-icon" uk-icon="icon: lock"></span>
+                                        <input class="uk-input home__banner__card__input" type="text" placeholder="Nhập lại mật khẩu...">
+                                    </div>
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <?php require "template-parts/layouts/footer.php"; ?>
--- template-parts/layouts/header.php
+++ template-parts/layouts/header.php
@@ -22,12 +22,38 @@
 <?php require "template-parts/commons/facebook_chat.php"; ?>
 <!--app-->
 <div id="app" class="uk-height-viewport uk-offcanvas-content uk-overflow-hidden uk-position-relative">
+<div id="offcanvas-tungleads" class="offTungleads" uk-offcanvas="overlay: true">
+    <div class="uk-offcanvas-bar uk-flex uk-flex-column">
+        <div class="uk-flex-1">
+            <ul class="uk-nav-default uk-nav-parent-icon" uk-nav="toggle: >.uk-icon">
+                <li class="uk-active"><a href="#" style="--background: url('images/icon_home.png')">Trang chủ</a></li>
+                <li><a href="" href="#" style="--background: url('images/icon_about.png')">Giới thiệu</a></li>
+                <li class="uk-parent">
+                    <a href="#" style="--background: url('images/icon_khoahoc.png')">Khóa học</a>
+                    <span class="uk-icon uk-position-top-right"></span>
+                    <ul class="uk-nav-sub">
+                        <li><a href="#">Khóa học google ads</a></li>
+                        <li><a href="#">Khóa học facebook Ads</a></li>
+                        <li><a href="#">Khóa học tiktok Ads</a></li>
+                        <li><a href="#">Khóa học SEO web</a></li>
+                    </ul>
+                </li>
+                <li><a href="#" style="--background: url('images/icon_thanhtoan.png')">Thanh toán</a></li>
+            </ul>
+        </div>
+        <div class="uk-section-xsmall uk-background-muted uk-text-center offTungleads__holtine">
+            <div class="uk-container">
+                <span class="offTungleads__holtine__txt">Hotline: 0966.0808.67</span>
+            </div>
+        </div>
+    </div>
+</div>
 <div class="header" uk-sticky>
     <div class="uk-container uk-padding-remove">
         <nav class="uk-navbar-container uk-navbar-transparent" uk-navbar>
 
             <div class="uk-navbar-left">
-                <a href="" class="uk-navbar-item header__iconMenu uk-hidden@m"><img src="images/icon_menu.png" alt=""></a>
+                <a href="" uk-toggle="target: #offcanvas-tungleads" class="uk-navbar-item header__iconMenu uk-hidden@m"><img src="images/icon_menu.png" alt=""></a>
                 <a href="" class="uk-navbar-item uk-logo uk-visible@m" uk-toggle="cls: uk-padding-remove-left; mode: media; media: @l"><img src="images/logo.png" alt=""></a>
                 <ul class="uk-navbar-nav uk-visible@m">
                     <li class="uk-active"><a href="#" data-src="images/icon_home.png" uk-img>Trang chủ</a></li>
